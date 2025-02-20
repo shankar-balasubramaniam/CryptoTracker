42357b4960fa774dabcb298f7a6af541763b17bf
Shankar <shankarb3061997@gmail.com>
2025-02-20 10:50:28 +0530

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Shankar Balasubramaniam on 20/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
